4457aae2bdb5578cd7b48eb68dd688ae72104630
Miguel Jimenez <denkatalabeno@gmail.com>
2021-10-21 14:56:33 +0800

    sending messages and handling errors!

--- sender.rb
+++ sender.rb
@@ -1,12 +1,42 @@
 require 'twilio-ruby'
+require 'pry-byebug'
+require 'csv'
 
-account_sid = 'AC08b9d5439ca5f6e0fce47f8d1043c58a'
-auth_token = '[AuthToken]'
-@client = Twilio::REST::Client.new(account_sid, auth_token)
+class Logger
+  def initialize
+    @file_path = 'logs.csv'
+  end
 
-message = @client.messages.create(
-                             messaging_service_sid: 'MGb10f3fda35f0d0ae66dc1e8b9ee017e3',
-                             to: '+6590370018'
-                           )
+  def report(phone_number, exception)
+    CSV.open(@file_path, "w") do |csv|
+      csv << [phone_number, exception]
+    end
+  end
+end
 
-puts message.sid
+
+class TwilioService
+  LOGGER = Logger.new
+  ACCOUNT_SID = 'AC08b9d5439ca5f6e0fce47f8d1043c58a'
+  AUTH_TOKEN = '0be822126371bee0f7114dd342000776'
+  MESSAGING_SERVICE_SID = 'MGb10f3fda35f0d0ae66dc1e8b9ee017e3'
+  CLIENT = Twilio::REST::Client.new(ACCOUNT_SID, AUTH_TOKEN)
+
+  def self.call(phone_number, sms_body)
+    message = CLIENT.messages.create(
+        messaging_service_sid: MESSAGING_SERVICE_SID,
+        body: sms_body,
+        to: phone_number
+    )
+  rescue => e
+    puts e
+    LOGGER.report(phone_number, e)
+  end
+end
+
+message = "This is a potato"
+numbers = ['+6590370018', '+6555555555']
+
+numbers.each do |num|
+  TwilioService.call(num, message)
+end
---
+++ todo_pei_ern.md
@@ -0,0 +1,13 @@
+
+# Making this ready for submission
+
+# -> Spread the exception across different columns
+# -> Interesting to add the point in time the exception was logged ( Time.now )
+
+# -> Make sure you are accumulating the logs over time
+
+
+# Gold Plate the submission
+
+# -> Add rspec tests to the the service
+# -> Look into how to mock an API in Rspec ( Miguel to send resources )
